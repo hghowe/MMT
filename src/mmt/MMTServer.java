eb7e828b202d4bf6d09f9bb7c767c2e1e73fb257
hghowe <hghowe@gmail.com>
2014-03-13 09:14:44 -0500

    Initial Commit

---
+++ src/mmt/MMTServer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmt;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author harlan.howe
+ */
+public class MMTServer extends TimerTask{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MMTServer theApp = new MMTServer();
+        theApp.go();
+    }
+    
+    public MMTServer()
+    {
+        super();
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(this,0,2000); // parameters: 0) which TimerTask 
+                                            // object's "run" method should I  
+                                            // call? 1) how many milliseconds  
+                                            // until I call it the first time?
+                                            // 2) how many milliseconds
+                                            // between subsequent calls?
+        
+    }
+    
+    public void go()
+    {
+        System.out.println("Starting Program.");
+    }
+    
+    public void run()
+    {
+        System.out.println("executing run() method");
+    }
+}
